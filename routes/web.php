06903bd63389fe536ac99339f506bf80b4e7d199
Victoria Gibbs <victoriahgibbs@gmail.com>
2025-07-24 17:01:32 -0400

    Routing completed and moving on to controllers

--- routes/web.php
+++ routes/web.php
@@ -30,9 +30,9 @@ use Illuminate\Support\Facades\Route;
 // });
 
 // Route Parameter validation
-Route::get('/product/{id}', function (string $id) {
-  return "Works! $id";
-})->whereNumber('id');
+// Route::get('/product/{id}', function (string $id) {
+//   return "Works! $id";
+// })->whereNumber('id');
 
 // can mix and match these or include a different one for each parameter. 
 // whereNumber() only digits
@@ -42,29 +42,77 @@ Route::get('/product/{id}', function (string $id) {
 
 
 // Custom Regex for validation
-Route::get(uri:'/user/{username}', action: function(string $username) {
-// ...
-})->where(name:'username', expression:'[a-z]+'); //requires only lowercase
+// Route::get(uri:'/user/{username}', action: function(string $username) {
+// // ...
+// })->where(name:'username', expression:'[a-z]+'); //requires only lowercase
 
-Route::get(uri:'{lang}/product/{id}', action: function(string $lang, string $id) {
-  // ...
-})->where(['lang' => '[a-z]{2}', 'id' => '\d{4,}']);
+// Route::get(uri:'{lang}/product/{id}', action: function(string $lang, string $id) {
+//   // ...
+// })->where(['lang' => '[a-z]{2}', 'id' => '\d{4,}']);
 
-Route::get(uri:'search/{search}', action: function(string $search){
-  return $search;
-})->where('search', '.+');
+// Route::get(uri:'search/{search}', action: function(string $search){
+//   return $search;
+// })->where('search', '.+');
 
 
 //Named Routes - associating a name to a route makes updating URLs super easy since you will only need to do it in one place instead of multiple. 
+// Route::get('/', function () {
+
+//   $productUrl = route('product.view',['lang' => 'en', 'id'=> 1]);
+//   dd($productUrl);
+
+//   return view('welcome');
+// });
+
+// Route::view(uri: '/about-us', view: 'about')->name(name:'about'); // editing the uri here will update the url anywhere the named route is used. 
+
+
+//Named Routes with parameters. Uses stuff above in the Named routes section as well
+// Route::get(uri:'p/{lang}/product/{id}', action: function(string $lang, string $id) {
+//   // ...
+// })->name(name:'product.view');
+
+// Route::get(uri:'/user/profile', action:function(){})->name(name: 'profile');
+
+// Route::get(uri:'/current-user', action:function(){
+//   // To ways to set up a redirect.
+//   return redirect()->route(route: 'profile');
+
+//   // return to_route(route: 'profile');
+// });
+
+// Route Groups 1:08:36
+
 Route::get('/', function () {
-  // $aboutPageUrl = '/about';
-  $aboutPageUrl = route(name:'about');
-  dd($aboutPageUrl);
   return view('welcome');
 });
 
-Route::view(uri: '/about', view: 'about')->name(name:'about'); // editing the uri here will update the url anywhere the named route is used. 
+Route::view(uri: '/about', view: 'about')->name(name:'about');
+
+Route::prefix('admin')->group(function () {
+  Route::get('/users', function(){
+    return '/admin/users';
+  });
+});
+
+Route::name('admin.')->group(function () {
+  Route::get('/users', function(){
+    return '/users'; //But the route name is "admin.users"
+  })->name('users');
+});
 
 
-//Named Routes with parameters. 1:06:04
+// Fallback Routes , used when there would normally be a 404 error. Can set up and use another view that will be shown anytime there would be a 404. 
 
+Route::fallback(function () {
+  return 'Fallback';
+});
+
+// View registered routes with Artisan in terminal
+// php artisan route:list  // gets all regardless of who made them
+// php artisan route:list --except-vendor   // to get any you have made
+// php artisan route:list -v  // gives details about the routes
+
+// Route Caching in terminal
+// php artisan route:cache  // Creates a cached version of the laravel routes. Will speed up and improve overall performance of the site. 
+// php artisan route:clear // Clears out any saved cache
